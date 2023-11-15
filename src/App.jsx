17bf9996fb68bbe8ec58c98c997517deac31b014
bsshady <konamipo4emy@gmail.com>
2023-11-15 23:45:31 +0200

    i-love-you

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import style from "./global.module.scss"
 const App =() => (
   <div className={style.wrapper}>
     <MainPage/>
-  </div> 
+    <div className={style.love}>i love you, KOTYA! :3</div>
+  </div>
 );
 
 export default App
